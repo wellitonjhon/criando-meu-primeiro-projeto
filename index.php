6e8ecbbc391f6e7d491b840c3f8f80eade6f53f8
welloton <welloton@jhon>
2017-07-17 17:12:37 -0300

    fORMULARIO DE LOGIN E MENU

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>home</title>    
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet"/>
+
+    </head>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Sistema</a>
+                </div>
+                <div class=" navbar-brand navbar-right">Olá teste. Seja Bem vindo!!</div>
+            </div>
+            <nav
+                <div class="container">
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs nav-pills" role="tablist">
+                        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+                        <li role="presentation"><a href="#pedidos" aria-controls="pedidos" role="tab" data-toggle="tab">Pedidos</a></li>
+                        <li role="presentation"><a href="#produtos" aria-controls="produtos" role="tab" data-toggle="tab">Produtos</a></li>
+                        <li role="presentation"><a href="#relatorios" aria-controls="relatorios" role="tab" data-toggle="tab">Relatórios</a></li>
+                        <li role="presentation"><a href="#manusis" aria-controls="manusis" role="tab" data-toggle="tab">Manuais</a></li>
+                        <li role="presentation"><a href="#configuracao" aria-controls="configuracao" role="tab" data-toggle="tab">Configurações</a></li>
+                        <li role="presentation" class="navbar-right"><a href="#compras" aria-controls="compras" role="tab" data-toggle="tab">Minhas Compras</a></li>
+                    </ul>
+
+                    <!-- Tab panes -->
+                    <div class="tab-content jumbotron">
+                        <div role="tabpanel" class="tab-pane active" id="home"><p>Lançamentos </p><hr>
+                            <div class="row">
+                                <div class="col-xs-4 col-md-3 ">
+                                    <a href="#" class="thumbnail">
+                                        <img src="imagem/fogao.jpg"   alt="...">
+                                    </a>
+                                    <div class="caption">
+                                        <div>fogão de piso electrolux de 5 bocas selebrate com doplo forno inox</div>
+                                        <h3>por R$839,90</h3>
+                                        <p><a href="#" class="btn btn-success btn-sm nav-justified" role="button">COMPRAR</a> </a></p>
+                                    </div>
+                                </div>
+                                <div class="col-xs-4 col-md-3">
+                                    <a href="#" class="thumbnail">
+                                        <img src="imagem/tv.jpg" alt="...">
+                                    </a>
+                                    <div class="caption">
+                                        <div>Smart tv sony 3D led 55 com android e wi-fi</div>
+                                        <h3>Por R$3.985,85</h3>
+                                        <p><a href="#" class="btn btn-success btn-sm nav-justified" role="button">COMPRAR</a> </p>
+                                    </div>
+                                </div>
+                                <div class="col-xs-4 col-md-3">
+                                    <a href="#" class="thumbnail">
+                                        <img src="imagem/lavadora.jpg" alt="...">
+                                    </a>
+                                    <div class="caption">
+                                        <div>Lavadora de roupas 10 kg</div>
+                                        <h3>Por R$1.896,90</h3>
+                                        <p><a href="#" class="btn btn-success btn-sm nav-justified" role="button">COMPRAR</a> </p>
+                                    </div>
+                                </div>
+                                <div class="col-xs-4 col-md-3">
+                                    <a href="#" class="thumbnail">
+                                        <img src="imagem/smartphone.jpg"  alt="bold">
+                                    </a>
+                                    <div class="caption">
+                                        <div>iphone 6 space, com tela de 4.7 4G 64GB e câmera de 8 </div>
+                                        <h3>Por R$3.299,89</h3>
+                                        <p><a href="#" class="btn btn-success btn-sm nav-justified" role="button">COMPRAR</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div role="tabpanel" class="tab-pane active" id="home">Oferstas Especiais <hr>
+                            <div class="row">
+                                <div class=" col-lg-3">produto</div>
+                                <div class="col-lg-3">produto</div>
+                                <div class="col-lg-3">produto</div>
+                                <div class="col-lg-3">produto</div>
+                            </div>
+                        </div>
+                        <div role="tabpanel" class="tab-pane active" id="home">Mais Vistos <hr>
+                            <div class="row">
+                                <div class=" panel col-lg-3">produto</div>
+                                <div class="col-lg-3">produto</div>
+                                <div class="col-lg-3">produto</div>
+                                <div class="col-lg-3">produto</div>
+                            </div>
+                        </div>
+                        <div role="tabpanel" class="tab-pane active" id="home">Você Visitou <hr>
+                            <div class="row">
+                                <div class=" panel col-lg-3">produto</div>
+                                <div class="col-lg-3">produto</div>
+                                <div class="col-lg-3">produto</div>
+                                <div class="col-lg-3">produto</div>
+                            </div>
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="pedidos">pedidos</div>
+                        <div role="tabpanel" class="tab-pane" id="produtos">produtos</div>
+                        <div role="tabpanel" class="tab-pane" id="relatorio">relatórios</div>
+                    </div>
+
+                </div>
+                <div class="jumbotron">
+                    <div class="container row">
+
+                        <div class="col-md-7">
+                            <ul class="nav nav-tabs nav-divider nav-justified">
+                                <li class="active"><a href="#Dashboard">Dashboard</a></li>
+                                <li class="active"><a href="#pedidos">Pedidos</a></li>
+                                <li class="active"><a href="#" >Produtos</a></li>
+                                <li class="active"><a href="#" >Relatórios</a></li>
+                                <li class="active"><a href="#" >Manuais</a></li>
+                                <li class="active"><a href="#" >Configurações</a></li>
+                            </ul>
+                        </div>
+                        <div class="tab-content">
+                            <div class="tab-pane active" id="Dashboard">Dashboard</div>
+                            <div class="tab-pane" id="pedidos">Pedidos</div>
+                            <div class="tab-pane" id="produtos">Produtos</div>
+                            <div class="tab-pane" id="relatorio">Relatório</div>
+                            <div class="tab-pane" id="manuais">Manuais</div>
+                            <div class="tab-pane" id="confiracoes"> Cofigurações</div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div class="container">
+                <div class="panel-info ">
+                    <div class="panel-heading">
+                        <h3 class="panel-title"><strong>Panel title</strong></h3>
+                    </div>
+                </div>            
+                <div class="panel-info">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Panel title</h3>
+                    </div>
+                </div>        
+                <div class="panel panel-primary">...</div>
+                <!-- =========================== javascript ====================================-->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+                <script src="/js/bootstrap.min.js"></script>
+                </body>
+                </html>
---
+++ modulo/introducao/classe.class.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * classe[TIPO]
+ * Descricao
+ * @copyright (c) year, welliton jhon Doe
+ */
+class classe {
+    //put your code here
+}
