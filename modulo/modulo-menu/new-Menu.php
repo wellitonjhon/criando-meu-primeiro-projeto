f9173d60cc088c9a62b2094648d1a33a94defd9f
welloton <welloton@jhon>
2017-07-18 20:38:22 -0300

    formulario de cadastro de cliente link da tela de login

---
+++ modulo/modulo-menu/new-Menu.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>menu</title>    
+        <link href="../../css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <nav class="navbar">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Sistema</a>
+                </div>
+                <div class=" navbar-brand navbar-right">Olá teste. Seja Bem vindo!!</div>
+            </div>
+            <nav
+                <div class="container">
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs nav-pills" role="tablist">
+                        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+                        <li role="presentation"><a href="#pedidos" aria-controls="pedidos" role="tab" data-toggle="tab">Pedidos</a></li>
+                        <li role="presentation"><a href="#produtos" aria-controls="produtos" role="tab" data-toggle="tab">Produtos</a></li>
+                        <li role="presentation"><a href="#relatorios" aria-controls="relatorios" role="tab" data-toggle="tab">Relatórios</a></li>
+                        <li role="presentation"><a href="#manusis" aria-controls="manusis" role="tab" data-toggle="tab">Manuais</a></li>
+                        <li role="presentation"><a href="#configuracao" aria-controls="configuracao" role="tab" data-toggle="tab">Configurações</a></li>
+                        <li role="presentation" class="navbar-right"><a href="../LoginUsuario.php" aria-controls="compras" role="tab" data-toggle="tab">Minhas Compras</a></li>
+                    </ul>
+                </div>
+        </nav>
+                </body>
+                </html>
