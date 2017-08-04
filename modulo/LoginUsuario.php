310f63bb7362607e02f1592fd34075d6a2fb8f48
welliton jhon da silva <wellitonjhon@hotmail.com>
2017-08-04 15:04:15 -0300

    Update LoginUsuario.php

--- modulo/LoginUsuario.php
+++ modulo/LoginUsuario.php
@@ -8,26 +8,26 @@
         <title>Login</title>    
         <link href="../css/bootstrap.css" rel="stylesheet">
         <link href="css/bootstrap.min.css" rel="stylesheet"/>
-
     </head>
     <body>
         <nav class="navbar">
             <div class="container">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#"><img src="../imagem/carro-de-compra.png"></a>Sistema</a>
-                </div>
+                    <a class="navbar-brand" href="#"><img src="../imagem/carro-de-compra.png"></a>Sistema</a></div>
                 <div class=" navbar-brand navbar-right">Olá teste. Seja Bem vindo!!</div>
             </div>
         </nav>
         <div class="container">
             <div class="row jumbotron">
                 <div class="col-xs-6 col-md-6">
-                    <p>Acesse sua conta</p>
+                        <p>Acesse sua conta</p>
+                    
                     <form class="container">
                         <div class="form-group">
                             <label for="Email">Endereço de e-mail</label>
                             <input type="email" class="form-control" id="Email" placeholder="Digite seu Email">
                         </div>
+                        
                         <div class="form-group">
                             <label for="Password">Você tem uma senha de solução integrada?</label>
                             <select class="form-control">
@@ -39,19 +39,20 @@
                                 </div>
                             </select>
                         </div>
+                        
                         <div class="form-group">
                             <label for="Password">Senha: - <a href="#">Esquecie minha senha</a></label>
                             <input type="password" class="form-control" id="Password" placeholder="Digite sua senha">
                         </div>
-
                         <button type="submit" class="btn btn-success">Continuar</button>
                     </form>
+                    
                 </div>
                 <!--============================Formulario de consulta de pedidos===================================-->
                 <div class="col-xs-6 col-md-6">
                     <p>Consultar pedidos</p>
+                    
                     <form class="container">
-
                         <div class="form-group">
                             <label for="pedido">Número do pedido</label>
                             <input type="text" class="form-control" id="pedidos" placeholder="Digite o número do seu pedido">
@@ -62,9 +63,8 @@
                         </div>
                         <button type="submit" class="btn btn-success">Consultar</button>
                     </form>
+
                 </div>
-
-
             </div>
         </div>
     </body>
